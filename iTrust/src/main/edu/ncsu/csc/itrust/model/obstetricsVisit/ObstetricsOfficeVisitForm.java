01e00400c6cfff3ef34782270a4827734035e747
Rose Isadore <rvisador@ncsu.edu>
2017-03-29 20:19:42 -0400

    Consolidated ObstetricsOfficeVisitForm.
    Altered tests to match current stated of the class.

--- iTrust/src/main/edu/ncsu/csc/itrust/model/obstetricsVisit/ObstetricsOfficeVisitForm.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/obstetricsVisit/ObstetricsOfficeVisitForm.java
@@ -1,120 +1,216 @@
 package edu.ncsu.csc.itrust.model.obstetricsVisit;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.servlet.http.Part;
+
+import edu.ncsu.csc.itrust.exception.DBException;
+import edu.ncsu.csc.itrust.model.obstetricsVisit.ObstetricsDataMySQL;
+import edu.ncsu.csc.itrust.model.obstetricsVisit.ObstetricsVisit;
+import edu.ncsu.csc.itrust.model.obstetricsVisit.ObstetricsVisitMySQL;
+import edu.ncsu.csc.itrust.webutils.SessionUtils;
 
 @ManagedBean(name = "obstetrics_office_visit_form")
 @ViewScoped
 public class ObstetricsOfficeVisitForm {
 	
-	private Long visitID;
-	private Long patientMID;
+	private long visitID;
+	private long patientMID;
+	private long weeksPregnant;
 	private LocalDateTime lastMenstrualPeriod;
 	private LocalDateTime expectedDeliveryDate;
 	private LocalDateTime date;
 	private Long apptTypeID;
-	private Float weight;
-	private String bloodPressure;
-	private long fetalHR;
-	private String multiple;
-	private Part imageFile;
-	private String filePath;
+	private float weight;
+	private int bloodPressure;
+	private int fetalHR;
+	private int pregnancies;
+	private ObstetricsVisitMySQL mySQL;
+	private ObstetricsVisit obstetricsOV;
+	private ObstetricsDataMySQL sql;
 	
+	
+	public Integer getPregnancies() {
+		return pregnancies;
+	}
+
+	public void setPregnancies(Integer pregnancies) {
+		this.pregnancies = pregnancies;
+	}
+	
+	public ObstetricsOfficeVisitForm() {
+		this.obstetricsOV = new ObstetricsVisit();
+		this.expectedDeliveryDate = LocalDateTime.now();
+		this.patientMID = SessionUtils.getInstance().getCurrentPatientMIDLong().longValue();
+		try {
+			this.mySQL = new ObstetricsVisitMySQL();
+		} catch (DBException e1) {
+			//Do nothing
+		}
+		
+		try {
+			this.sql = new ObstetricsDataMySQL();
+			this.lastMenstrualPeriod = this.sql.getLmpDate(patientMID);
+		} catch (DBException e) {
+			//Do nothing
+		}
+	}
+	
+	public ObstetricsOfficeVisitForm(long patientMID) {
+		this.obstetricsOV = new ObstetricsVisit();
+		this.expectedDeliveryDate = LocalDateTime.now();
+		this.patientMID = patientMID;
+		try {
+			this.mySQL = new ObstetricsVisitMySQL();
+		} catch (DBException e1) {
+			//Do nothing
+		}
+		
+		try {
+			this.sql = new ObstetricsDataMySQL();
+			this.lastMenstrualPeriod = this.sql.getLmpDate(patientMID);
+		} catch (DBException e) {
+			//Do nothing
+		}
+	}
+	
+	public void getVisitDate() {
+		Long pid = Long.parseLong(SessionUtils.getInstance().getSessionPID());
+		this.date = this.mySQL.getDateOfVisit(pid);
+		this.lastMenstrualPeriod = sql.getLmpDate(pid);
+		
+		if (this.date == null) {
+			this.date = LocalDateTime.now();
+		}
+		if (this.lastMenstrualPeriod == null) {
+			this.lastMenstrualPeriod = LocalDateTime.now();
+		}
+		
+//		System.out.println(this.date.toLocalDate().toString() + "     " + this.lastMenstrualPeriod.toLocalDate().toString());
+	}
+
+
 	public Long getVisitID() {
 		return visitID;
 	}
+
 	public void setVisitID(Long visitID) {
 		this.visitID = visitID;
 	}
+
 	public Long getPatientMID() {
 		return patientMID;
 	}
+
 	public void setPatientMID(Long patientMID) {
 		this.patientMID = patientMID;
 	}
+
+	public Long getWeeksPregnant() {
+		return weeksPregnant;
+	}
+
+	public void setWeeksPregnant(Long weeksPregnant) {
+		this.weeksPregnant = weeksPregnant;
+	}
+
+	public String getLastMenstrualPeriodString() {
+		if (lastMenstrualPeriod != null) {
+			return lastMenstrualPeriod.toLocalDate().toString();
+		}
+		else {
+			return "";
+		}
+	}
+
 	public LocalDateTime getLastMenstrualPeriod() {
 		return lastMenstrualPeriod;
 	}
+	
 	public void setLastMenstrualPeriod(LocalDateTime lastMenstrualPeriod) {
 		this.lastMenstrualPeriod = lastMenstrualPeriod;
 	}
-	public LocalDateTime getExpectedDeliveryDate() {
-		return expectedDeliveryDate;
+
+	public String getExpectedDeliveryDate() {
+		return expectedDeliveryDate.toLocalDate().toString();
+		
 	}
+
 	public void setExpectedDeliveryDate(LocalDateTime expectedDeliveryDate) {
 		this.expectedDeliveryDate = expectedDeliveryDate;
 	}
+
 	public LocalDateTime getDate() {
 		return date;
+	}
+
+	public LocalDate getDateString() {
+		return date.toLocalDate();
 	}
 	public void setDate(LocalDateTime date) {
 		this.date = date;
 	}
+
 	public Long getApptTypeID() {
 		return apptTypeID;
 	}
+
 	public void setApptTypeID(Long apptTypeID) {
 		this.apptTypeID = apptTypeID;
 	}
+
 	public Float getWeight() {
 		return weight;
 	}
+
 	public void setWeight(Float weight) {
 		this.weight = weight;
 	}
-	public String getBloodPressure() {
+
+	public Integer getBloodPressure() {
 		return bloodPressure;
 	}
-	public void setBloodPressure(String bloodPressure) {
+
+	public void setBloodPressure(Integer bloodPressure) {
 		this.bloodPressure = bloodPressure;
 	}
-	public long getFetalHR() {
+
+	public Integer getFetalHR() {
 		return fetalHR;
 	}
-	public void setFetalHR(long fetalHR) {
+
+	public void setFetalHR(Integer fetalHR) {
 		this.fetalHR = fetalHR;
 	}
-	public String getMultiple() {
-		return multiple;
-	}
-	public void setMultiple(String multiple) {
-		this.multiple = multiple;
-	}
-	public Part getImageFile() {
-		return imageFile;
-	}
-	public void setImageFile(Part imageFile) {
-		this.imageFile = imageFile;
-	}
-	public String getFilePath() {
-		return filePath;
-	}
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
+
+
 	
 	public void save() {
-		try( InputStream input = imageFile.getInputStream() ) {
-			System.out.println(new File("/iTrust").getAbsolutePath());
-			Files.copy(input, new File(".", imageFile.getSubmittedFileName()).toPath());			
-			System.out.println(imageFile.getSubmittedFileName());
+//		try( InputStream input = imageFile.getInputStream() ) {
+//			Files.copy(input, new File("C:/Users/Jacob/git/csc326-203-Project-01/iTrust/images/", imageFile.getSubmittedFileName()).toPath());			System.out.println(imageFile.getSubmittedFileName());
+//			System.out.println(imageFile.getSubmittedFileName());
 //			imageFile.write("/iTrust/images/" + imageFile.getSubmittedFileName());
-		} catch (IOException e){
-			e.printStackTrace();
-			System.out.println(e.getMessage());
+//		} catch (IOException e){
+//			e.printStackTrace();
+//			System.out.println(e.getMessage());
 			//not sure what to say, something like "file does not exist or couldn't be uploaded
-		}
+//		}
 	}
 	
 	public void submit() {
+		this.obstetricsOV.setWeight(weight);
+		this.obstetricsOV.setBloodPressure(bloodPressure);
+		this.obstetricsOV.setFetalHeartRate(fetalHR);
+		this.obstetricsOV.setPregnancies(pregnancies);
 		
+		try {
+			this.mySQL.addObstetricsVisit(obstetricsOV);
+		} catch (DBException e) {
+			//Do Nothing
+		}
 	}
-	
-	
 }
--- iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetricsVisit/ObstetricsOfficeVisitFormTest.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetricsVisit/ObstetricsOfficeVisitFormTest.java
@@ -15,10 +15,9 @@ public class ObstetricsOfficeVisitFormTest {
 	private static final long ID = 1l;
 	private static final long MID = 102l;
 	private static final float WEIGHT = 185.23f;
-	private static final String BLOOD_PRESSURE = "52";
-	private static final long FETAL_HEART_RATE = 73l;
-	private static final String PREGNANCIES = "8";
-	private static final String PATH = "This is totally a path.";
+	private static final int BLOOD_PRESSURE = 52;
+	private static final int FETAL_HEART_RATE = 73;
+	private static final int PREGNANCIES = 8;
 	private static final LocalDateTime DATE1 = LocalDateTime.of(2017, 01, 01, 0, 0);
 	private static final LocalDateTime DATE2 = LocalDateTime.of(2017, 02, 02, 0, 0);
 	private static final LocalDateTime DATE3 = LocalDateTime.of(2017, 03, 03, 0, 0);
@@ -26,14 +25,13 @@ public class ObstetricsOfficeVisitFormTest {
 	
 	@Test
 	public void testObstetricsData() {
-		ov = new ObstetricsOfficeVisitForm();
+		ov = new ObstetricsOfficeVisitForm(99l);
 		ov.setVisitID(ID);
 		ov.setPatientMID(MID);
 		ov.setWeight(WEIGHT);
 		ov.setBloodPressure(BLOOD_PRESSURE);
 		ov.setFetalHR(FETAL_HEART_RATE);
-		ov.setMultiple(PREGNANCIES);
-		ov.setFilePath(PATH);
+		ov.setPregnancies(PREGNANCIES);
 		ov.setLastMenstrualPeriod(DATE1);
 		ov.setDate(DATE2);
 		ov.setExpectedDeliveryDate(DATE3);
@@ -41,14 +39,13 @@ public class ObstetricsOfficeVisitFormTest {
 		
 		Assert.assertEquals(DATE1, ov.getLastMenstrualPeriod());
 		Assert.assertEquals(DATE2, ov.getDate());
-		Assert.assertEquals(DATE3, ov.getExpectedDeliveryDate());
+		Assert.assertEquals(DATE3.toLocalDate().toString(), ov.getExpectedDeliveryDate().toString());
 		assertEquals("1", ov.getVisitID().toString());
 		assertEquals("102", ov.getPatientMID().toString());
 		assertEquals("185.23", ov.getWeight().toString());
-		assertEquals("52", ov.getBloodPressure());
-		assertEquals(73, ov.getFetalHR());
-		assertEquals("8", ov.getMultiple());
-		assertEquals("This is totally a path.", ov.getFilePath());
+		assertEquals("52", ov.getBloodPressure().toString());
+		assertEquals("73", ov.getFetalHR().toString());
+		assertEquals("8", ov.getPregnancies().toString());
 		assertEquals("7", ov.getApptTypeID().toString());
 	}
 }
