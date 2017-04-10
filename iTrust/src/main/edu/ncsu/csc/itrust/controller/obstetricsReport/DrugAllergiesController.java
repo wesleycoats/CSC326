7c16bdef4a122b63acb7124e698a2f4bb62381d9
Rose Isadore <rvisador@ncsu.edu>
2017-04-10 12:54:53 -0400

    Started creating the skeleton that the report will be based on.

---
+++ iTrust/src/main/edu/ncsu/csc/itrust/controller/obstetricsReport/DrugAllergiesController.java
@@ -0,0 +1,16 @@
+package edu.ncsu.csc.itrust.controller.obstetricsReport;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import edu.ncsu.csc.itrust.controller.iTrustController;
+
+@ManagedBean(name = "drug_allergies")
+@SessionScoped
+public class DrugAllergiesController extends iTrustController {
+
+	public DrugAllergiesController() {
+
+	}
+
+}
